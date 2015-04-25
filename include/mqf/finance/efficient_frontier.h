e46ec64e595d351f19c21081f82b0c7bdc0021eb
cwzx <chriswelshman@gmail.com>
2015-04-25 21:27:23 +0100

    asd

--- include/mqf/finance/efficient_frontier.h
+++ include/mqf/finance/efficient_frontier.h
@@ -1,10 +1,19 @@
 #ifndef INCLUDED_MQF_EFFICIENT_FRONTIER
 #define INCLUDED_MQF_EFFICIENT_FRONTIER
 #include <Eigen/Core>
-#include <Eigen/LU>
+#include "../eigen_pinv.h"
 
 namespace mqf {
 
+	/*
+	 * Efficient Frontier
+	 *
+	 * The set of portfolios that have the least variance for their expected return.
+	 *
+	 * The tangency portfolio is the portfolio on the efficient frontier
+	 * that maximizes the Sharpe ratio w.r.t. a given riskFreeRate.
+	 *
+	 */
 	struct EfficientFrontier {
 		using Vec = Eigen::VectorXd;
 		using Mat = Eigen::MatrixXd;
@@ -16,26 +25,26 @@ namespace mqf {
 
 		void set( const Vec& returns, const Mat& covariance ) {
 			assetReturns = returns;
-			assetCovarianceInverse = Eigen::FullPivLU<Mat>(covariance).inverse();
-			double a = computeA();
-			double b = computeB();
-			double c = computeC();
-			double d = computeD();
+			assetCovarianceInverse = pseudoInverse(covariance);
+			a = computeA();
+			b = computeB();
+			c = computeC();
+			d = computeD();
 		}
 
 		void setReturns( const Vec& ret ) {
 			assetReturns = ret;
-			double a = computeA();
-			double b = computeB();
-			double d = computeD();
+			a = computeA();
+			b = computeB();
+			d = computeD();
 		}
 
 		void setCovariance( const Mat& covariance ) {
-			assetCovarianceInverse = Eigen::FullPivLU<Mat>(covariance).inverse();
-			double a = computeA();
-			double b = computeB();
-			double c = computeC();
-			double d = computeD();
+			assetCovarianceInverse = pseudoInverse(covariance);
+			a = computeA();
+			b = computeB();
+			c = computeC();
+			d = computeD();
 		}
 
 		double minVariance() const {
@@ -44,6 +53,10 @@ namespace mqf {
 
 		double returnForMinVariance() const {
 			return a / c;
+		}
+
+		double returnOfTangency( double riskFreeRate = 0.0 ) const {
+			return ( riskFreeRate * a - b ) / ( riskFreeRate * c - a );
 		}
 
 		double varianceForReturn( double mu ) const {
@@ -62,6 +75,10 @@ namespace mqf {
 			auto g = assetCovarianceInverse * ( Vec::Ones(N) * b - assetReturns * a ) / d;
 			auto h = assetCovarianceInverse * ( assetReturns * c - Vec::Ones(N) * a ) / d;
 			return g + h * (a / c);
+		}
+
+		Vec computeTangencyPortfolio( double riskFreeRate = 0.0 ) const {
+			return computePortfolioWithReturn( returnOfTangency(riskFreeRate) );
 		}
 
 		double computeA() const {
